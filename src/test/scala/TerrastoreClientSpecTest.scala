5276eb6b4afd8b358e2acef476acb10919c590fc
unknown <surya.suravarapu@.NA.AD.VWR.COM>
2010-05-19 11:57:45 -0400

    Changes to make Specs work

--- src/test/scala/TerrastoreClientSpecTest.scala
+++ src/test/scala/TerrastoreClientSpecTest.scala
@@ -1,4 +1,5 @@
 import org.specs._
+import runner.ScalaTest
 
 /**
  * Created by IntelliJ IDEA.
@@ -8,19 +9,20 @@ import org.specs._
  * To change this template use File | Settings | File Templates.
  */
 
-object TerrastoreClientSpec extends Specification {
+class TerrastoreClientSpecTest extends SpecificationWithJUnit {
 
   "Put a new document" should {
     "add the document specified in the bucket specified" in {
       val client:TerrastoreClient = new TerrastoreClient("localhost", 8010)
-      client.putDocument("SPS1", "key1", "{\"color\":\"red\", \"value\":\"#f00\"}")
+      client.putDocument("SPS2", "some-key", "{\"color\":\"red\", \"value\":\"#f00\"}")
     }
   }
 
   "Get the bucket list" should {
     "have SPS1 in it" in {
       val client:TerrastoreClient = new TerrastoreClient("localhost", 8010)
-      "SPS1" must beOneOf(client.getBucketNames)
+      val bucketNames = client.getBucketNames
+      bucketNames must contain("SPS2")
     }
   }
 }
