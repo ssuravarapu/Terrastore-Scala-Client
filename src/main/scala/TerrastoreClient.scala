70cadd09d2a0ecafeaf9bf53d4944f7da6c53ec9
unknown <surya.suravarapu@.NA.AD.VWR.COM>
2010-05-21 13:41:01 -0400

    Cleanup!

--- src/main/scala/TerrastoreClient.scala
+++ src/main/scala/TerrastoreClient.scala
@@ -5,11 +5,13 @@ import dispatch.{Request, :/, Http}
 import util.parsing.input.CharSequenceReader
 import util.parsing.json.JSON
 
-class TerrastoreClient (hostName: String, port: Int) {
+case class TerrastoreClient (hostName: String, port: Int) {
+  val http = new Http
+  val req = :/(hostName, port)
+
+  implicit val formats = net.liftweb.json.DefaultFormats
 
   def getBucketNames : List[String] = {
-    val http = new Http
-    val req = :/(hostName, port)
     val res = http(req as_str)
     JSON.parse(res) match {
       case Some(list) => list map {_.toString} 
@@ -25,43 +27,25 @@ class TerrastoreClient (hostName: String, port: Int) {
   }
 
   def removeBucket (bucketName:String) = {
-    val http = new Http
-    val req = :/(hostName, port)
     http(req.DELETE / bucketName >|)
   }
 
   def putValue (bucket:String, key: String, content:String) = {
-      val http = new Http
-      val req = :/(hostName, port) / bucket / key <<< content <:< Map("Content-Type"-> "application/json")
-      val res = http (req >|)
+    http(req / bucket / key <<< content <:< Map("Content-Type"-> "application/json") >|)
   }
 
   def removeValue (bucket:String, key:String) = {
-    val http = new Http
-    val req = :/(hostName, port)
-    http(req.DELETE / bucket / key >|)  
+    http(req.DELETE / bucket / key >|)
   }
 
   def getValue[T](bucket: String, key: String)(implicit formats: Formats, mf: scala.reflect.Manifest[T]) : T = {
-    val http = new Http
-    val req = :/(hostName, port) / bucket / key
-    val res = http(req as_str)
+    val res = http(req / bucket / key as_str)
     println("res: " + res)
-    implicit val formats = net.liftweb.json.DefaultFormats
     parse(res).extract[T]
   }
 
-  def getValue2 (bucket: String, key: String) : Any = {
-    val http = new Http
-    val req = :/(hostName, port) / bucket / key
-    val res = http(req as_str)
-    JsonParser (new CharSequenceReader(res))
-  }
-
   def getAllValues (bucket:String):List[Any] = {
-    val http = new Http
-    val req = :/(hostName, port) / bucket
-    val res = http(req as_str)
+    val res = http(req / bucket as_str)
     JSON.parse(res) match {
       case Some(s) => s
       case None => List.empty
--- src/test/scala/TerrastoreClientSpecTest.scala
+++ src/test/scala/TerrastoreClientSpecTest.scala
@@ -1,18 +1,16 @@
 import dispatch.StatusCode
 import java.lang.String
-import net.liftweb.json.JsonParser._
 import org.specs._
-import reflect.BeanInfo
 
 class TerrastoreClientSpecTest extends SpecificationWithJUnit {
+  implicit val formats = net.liftweb.json.DefaultFormats
+  val client = TerrastoreClient("localhost", 8010)
   val bucketName = "XYZ1234"
   val key = "person1"
-  val value = "{\"color\" : \"red\", \"value\" : \"#f00\"}"
   val jsonStr = "{\"name\": \"Surya Suravarapu\", \"address\": {\"street\": \"622 Sunderland\",\"city\": \"Chester Springs\"}}"
 
   "Put a new document" should {
     "add the document specified in the bucket specified" in {
-      val client:TerrastoreClient = new TerrastoreClient("localhost", 8010)
       client.putValue(bucketName, key, jsonStr)
       client.getBucketNames must contain(bucketName)
     }
@@ -20,8 +18,6 @@ class TerrastoreClientSpecTest extends SpecificationWithJUnit {
 
   "Get a document" should {
     "have the value specified" in {
-      val client:TerrastoreClient = new TerrastoreClient("localhost", 8010)
-      implicit val formats = net.liftweb.json.DefaultFormats
       val person = client.getValue[Person](bucketName, key)
       person.name must equalIgnoreSpace("Surya Suravarapu")
       person.address.street must equalIgnoreSpace("622 Sunderland")
@@ -31,7 +27,6 @@ class TerrastoreClientSpecTest extends SpecificationWithJUnit {
 
   "Get the bucket list" should {
     "have SPS1 in it" in {
-      val client:TerrastoreClient = new TerrastoreClient("localhost", 8010)
       val bucketNames = client.getBucketNames
       bucketNames must contain(bucketName)
     }
@@ -39,16 +34,13 @@ class TerrastoreClientSpecTest extends SpecificationWithJUnit {
 
   "Delete a value" should {
     "remove the value specified" in {
-      val client:TerrastoreClient = new TerrastoreClient("localhost", 8010)
       client.removeValue(bucketName, key)
-      implicit val formats = net.liftweb.json.DefaultFormats
       client.getValue[Person](bucketName, key) must throwA[StatusCode]
     }
   }
 
   "Delete a bucket" should {
     "remove the bucket specified" in {
-      val client:TerrastoreClient = new TerrastoreClient("localhost", 8010)
       client.removeBucket(bucketName)
       client.getBucketNames must not contain bucketName
     }
