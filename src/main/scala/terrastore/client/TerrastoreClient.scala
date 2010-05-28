736e3406176bb9d8dbe2c051c97d02f725b2613d
Surya Suravarapu <surya.suravarapu@.NA.AD.VWR.COM>
2010-05-28 14:48:15 -0400

    Refactored httpExecute to account for any response types.

--- src/main/scala/terrastore/client/TerrastoreClient.scala
+++ src/main/scala/terrastore/client/TerrastoreClient.scala
@@ -4,7 +4,7 @@ import net.liftweb.json.JsonParser._
 import net.liftweb.json.JsonAST._
 import net.liftweb.json.{Formats}
 import util.parsing.json.JSON
-import dispatch.{StatusCode, Handler, :/, Http}
+import dispatch._
 
 case class TerrastoreClient (hostName: String, port: Int) {
   val http = new Http
@@ -13,7 +13,7 @@ case class TerrastoreClient (hostName: String, port: Int) {
   implicit val formats = net.liftweb.json.DefaultFormats
 
   def getBucketNames : List[String] = {
-    val res = httpExecute(req as_str)
+    val res = httpExecute[String](req as_str)
     JSON.parse(res) match {
       case Some(list) => list map {_.toString}
       case None => List.empty
@@ -21,24 +21,24 @@ case class TerrastoreClient (hostName: String, port: Int) {
   }
 
   def removeBucket (bucketName:String) = {
-    http(req.DELETE / bucketName >|)
+    httpExecute[Unit](req.DELETE / bucketName >|)
   }
 
   def putDocument (bucket:String, key: String, content:String) = {
-    http(req / bucket / key <<< content <:< Map("Content-Type" -> "application/json") >|)
+    httpExecute[Unit](req / bucket / key <<< content <:< Map("Content-Type" -> "application/json") >|)
   }
 
   def removeDocument (bucket:String, key:String) = {
-    http(req.DELETE / bucket / key >|)
+    httpExecute[Unit](req.DELETE / bucket / key >|)
   }
 
   def getDocument[T](bucket: String, key: String)(implicit formats: Formats, mf: scala.reflect.Manifest[T]) : T = {
-    val res = httpExecute(req / bucket / key as_str)
+    val res = httpExecute[String](req / bucket / key as_str)
     parse(res).extract[T]
   }
 
   def getAllDocuments[T](bucket:String, limit:Int)(implicit formats: Formats, mf: scala.reflect.Manifest[T]) : Map[String, T] = {
-    val res = httpExecute(req / bucket <<? Map("limit" -> limit) as_str)
+    val res = httpExecute[String](req / bucket <<? Map("limit" -> limit) as_str)
     Map() ++ parse(res).children.map {
       case f: JField => (f.name, f.extract[T])
     }
@@ -47,7 +47,7 @@ case class TerrastoreClient (hostName: String, port: Int) {
   def doRangeQuery[T](bucket:String, startKey:String, endKey:String, limit:Int, comparator:String, predicate:String,
           timeToLive:Long)(implicit formats: Formats, mf: scala.reflect.Manifest[T]):Map[String, T] = {
     val params = Map ("startKey" -> startKey, "endKey" -> endKey, "limit" -> limit)
-    val res = httpExecute(req / bucket / "range" <<? params as_str)
+    val res = httpExecute[String](req / bucket / "range" <<? params as_str)
     Map() ++ parse(res).children.map {
       case f: JField => (f.name, f.extract[T])
     }
@@ -55,28 +55,27 @@ case class TerrastoreClient (hostName: String, port: Int) {
 
   def doPredicateQuery[T](bucket:String, predicate:String)
       (implicit formats: Formats, mf: scala.reflect.Manifest[T]):Map[String, T] = {
-    val res = httpExecute(req / bucket / "predicate" <<? Map("predicate" -> predicate) as_str)
+    val res = httpExecute[String](req / bucket / "predicate" <<? Map("predicate" -> predicate) as_str)
     Map() ++ parse(res).children.map {
       case f: JField => (f.name, f.extract[T])
     }
   }
 
   def exportBackup(bucket:String, destination:String, secret:String) = {
-    http(req.POST / bucket / "export" <<? Map("destination" -> destination, "secret" -> secret) <:<
+    httpExecute[Unit](req.POST / bucket / "export" <<? Map("destination" -> destination, "secret" -> secret) <:<
             Map("Content-Type" -> "application/json") >|)
   }
 
   def importBackup(bucket:String, source:String, secret:String) = {
-    http(req.POST / bucket / "import" <<? Map("source" -> source, "secret" -> secret) <:<
+    httpExecute[Unit](req.POST / bucket / "import" <<? Map("source" -> source, "secret" -> secret) <:<
             Map("Content-Type" -> "application/json") >|)
   }
 
-  private def httpExecute(handler:Handler[String]) = {
+  private def httpExecute[T](handler:Handler[T]):http.HttpPackage[T] = {
     try {
       http(handler)
     } catch {
       case e: StatusCode => {
-        println(e.contents)
         val error = parse(e.contents).extract[Error]
         throw new TerrastoreException(error)
       }
